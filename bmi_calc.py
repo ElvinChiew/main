d6bed15c906c962b6a8556178455ebc561727ccb
ElvinChiew <elvin_chiew@hotmail.com>
2022-09-14 22:10:52 +0800

    Changing gender=int(input()) and you typo

--- bmi_calc.py
+++ bmi_calc.py
@@ -4,11 +4,11 @@ print("Select Gender : ")
 print("1. Male")
 print("2. Female")
 
-input(gender)
+gender = int(input())
 
 if gender == 1 :
   print("You are male")
 elif gender == 2 :
-  print("Upu are Female")
+  print("You are Female")
 else:
   print("Wrong input")
