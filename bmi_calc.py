58a9a233aeb123db609814bc9b8597ee50be418f
ElvinChiew <elvin_chiew@hotmail.com>
2022-09-16 23:42:10 +0800

    Adding other BMI criteria

--- bmi_calc.py
+++ bmi_calc.py
@@ -16,6 +16,19 @@ else:
 def calculate_BMI(w, h):
   bmi = w / (h*h)
   bmi = round(bmi,2)
+
+  if bmi < 18.5 :
+    print("You are  underweight")
+  if bmi >= 18.5 and bmi <= 24.9 :
+    print("Your BMI are  normal")
+  if bmi >= 25 and bmi <= 29.9 :
+    print("You are overweight")
+  if bmi >= 30 and bmi <= 40 :
+    print("You are  Obese Type 1 (obese)")
+  if bmi >= 40.1 and bmi <= 50 :
+    print("You are  Obese Type 2 (morbid obese)")
+  if bmi >= 50 :
+    print("You are  Obese Type 3 (super obese)")
   return bmi
 
 print("Enter you height in m :")
